f52c6e5d1ea82374b5a2ed2a622361c3725a35c4
ivonaaaa <ivonaa.e@gmail.com>
2025-05-05 19:49:40 +0200

    Add Swagger setup, Prisma integration, and initial module structure

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Bay Obey API')
+    .setDescription('The API for the Internship CUP project')
+    .setVersion('1.0')
+    .addTag('bayobey')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
