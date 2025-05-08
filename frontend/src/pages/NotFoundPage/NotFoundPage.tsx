92092e0e6d323d9d31846ad0887d2de3183b7b80
Ivor <ikojun00@fesb.hr>
2025-05-08 23:33:14 +0200

    not found page

---
+++ frontend/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./NotFoundPage.module.css";
+
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className={styles.notFoundPage}>
+      <h2>404</h2>
+      <p>Page not found</p>
+      <button onClick={handleClick}>Homepage</button>
+    </div>
+  );
+};
+
+export default NotFoundPage;
