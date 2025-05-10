0e5a6b4096cec50b1d1087acef1d7208ccdb5d93
zulja1 <luka.zuljo@gmail.com>
2025-05-10 14:57:10 +0200

    Add main layout and style

--- frontend/src/pages/InfoPage/InfoPage.tsx
+++ frontend/src/pages/InfoPage/InfoPage.tsx
@@ -1,21 +1,47 @@
-//Obaveze javljanja o izvanrednim situacijama
-
-//UPRAVLJANJE BRODOVIMA PRI BILO KOJEM STANJU VIDLJIVOSTI
-
-//Red u lukama i na moru
-
-//Izbjegavanje sudara
-
 import c from "./InfoPage.module.css";
+import { InfoSection } from "../../components/InfoSection";
+import { useState } from "react";
+import homeButton from "../../assets/Home.png";
+import laws from "./laws.json";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../constants";
+
+type InfoSectionType = {
+  title: string;
+  content: string;
+};
 
 export const InfoPage = () => {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const handleSectionClick = (index: number) => {
+    setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
+  const navigate = useNavigate();
+
   return (
     <div className={c.infoPage}>
-      <div className={c.infoPageSection}></div>
-      <div className={c.infoPageSection}></div>
-      <div className={c.infoPageSection}></div>
-      <div className={c.infoPageSection}></div>
-      <div className={c.infoPageSection}></div>
+      <h1 className={c.infoPageHeader}>Rules and help</h1>
+      <div className={c.infoPageContent}>
+        {laws.map((section: InfoSectionType, index: number) => (
+          <InfoSection
+            key={index}
+            title={section.title}
+            content={section.content}
+            isOpen={openIndex === index}
+            onClick={() => handleSectionClick(index)}
+          />
+        ))}
+      </div>
+      <img
+        className={c.homeButton}
+        src={homeButton}
+        onClick={() => {
+          navigate(ROUTES.HOME);
+        }}
+        alt="Home"
+      />
     </div>
   );
 };
