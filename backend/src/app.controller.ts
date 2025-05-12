c434cb57df1267116afc466dbfa555e0f9be98a0
zulja1 <luka.zuljo@gmail.com>
2025-05-13 00:19:10 +0200

    Fix

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Res, Req } from '@nestjs/common';
 import { join } from 'path';
-import { Response } from 'express';
+import { Response, Request } from 'express';
 
 @Controller()
 export class AppController {
   @Get('*')
-  serveFrontend(@Res() res: Response) {
+  serveFrontend(@Req() req: Request, @Res() res: Response) {
+    if (req.url.startsWith('/api')) {
+      res.status(404).send('Not Found');
+      return;
+    }
+
     res.sendFile(
       join(__dirname, '..', '..', '..', 'frontend', 'dist', 'index.html'),
     );
